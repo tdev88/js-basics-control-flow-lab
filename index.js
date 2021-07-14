585d424496d2cf094ef1fa3d8f974bd8fda9caae
tdev88 <tracy.dev@outlook.com>
2021-07-14 20:54:07 +1000

    update

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(a) { 
+  if (a<=199) {
+    return (`This one is on me!`) 
+  }
+   else if (a>2500) {
+    return ('No can do.')
+  }
+  else if (a>=2001) {
+    return ('I will gladly take your thirty bucks.')
+  }  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city==='NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+        return 'Thank you so much.';
+        break;
+    case 'not as generous':
+        return 'Thank you.';
+        break;
+    default:
+        return 'Bye.';
+        break;
+  }
 }
